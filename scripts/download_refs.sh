c43136057996b8c9c66ba2d7f614e7a9699b6fc6
Katharine S. Walter <ksw9@users.noreply.github.com>
2023-01-19 17:45:22 -0700

    Update download_refs.sh

--- scripts/download_refs.sh
+++ scripts/download_refs.sh
@@ -4,7 +4,8 @@
 #### Download references & index reference genomes for Mtb variant calling pipeline ####
 ########################################################################################
 
-# Requires: entrez-direct (conda), bwa, GATK, samtools, kraken2
+# Define local container tool (default is docker).
+container=${1:-"docker"}   
 
 # Define locations
 ref_dir=resources/
@@ -12,19 +13,22 @@ mkdir -p ${ref_dir}
 cd ${ref_dir}
 ref_path=${ref_dir}/refs/H37Rv.fasta
 ncbi_id="NC_000962.3"
+image="ksw9/mtb-call"
 
+# Requires: entrez-direct (conda), bwa, GATK, samtools, kraken2, all present in the container.
+# Run each step within the image. Need to mount local directory so that the resources are downloaded locally, not just in the container.
 ## 1. Reference genome ##
 # Download H37Rv reference fasta (alternatively, use efetch)
-esearch -db nucleotide -query ${ncbi_id} | efetch -format fasta > ${ref_path}
+${container} run -v $(pwd)/${ref_dir} esearch -db nucleotide -query ${ncbi_id} | efetch -format fasta > ${ref_path}
 
 # bwa index reference
-bwa index ${ref_path}
+${container} run -v $(pwd)/${ref_dir} bwa index ${ref_path}
 
 # Create fasta index file
-samtools faidx ${ref_path}
+${container} run -v $(pwd)/${ref_dir}  samtools faidx ${ref_path}
 
 # create GATK reference dictionary
-gatk CreateSequenceDictionary -R ${ref_path}
+${container} run -v $(pwd)/${ref_dir} gatk CreateSequenceDictionary -R ${ref_path}
 
 ## 2. Masking bed file ##
 # PPE masking bed file (H37Rv_ppe.bed.gz) and VCF PPE annotation (ppe_hdr.txt) are in resources/bed hosted on Github
@@ -32,15 +36,15 @@ gatk CreateSequenceDictionary -R ${ref_path}
 ## 3. Annotation information ##
 # Download SnpEff for gene annotation.
 cd ${ref_dir}/snpEff
-wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
+${container} run -v $(pwd)/${ref_dir} wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
 
 # Unzip file
 unzip snpEff_latest_core.zip
 
 # Download the updated M. tuberculosis annotations.
-java -jar snpEff.jar download Mycobacterium_tuberculosis_h37rv
+${container} run -v $(pwd)/${ref_dir} java -jar snpEff.jar download Mycobacterium_tuberculosis_h37rv
 
 ## 4. Kraken2 Database ##
 # download kraken2 database (requires ~100G) 
 # https://github.com/DerrickWood/kraken2/blob/master/docs/MANUAL.markdown
-kraken2-build --standard --threads 24 --db $DBNAME
+${container} run -v $(pwd)/${ref_dir} kraken2-build --standard --threads 24 --db $DBNAME
